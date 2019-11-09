a5b7b71f26bbce3306cbd90be3fe47f26043dffa
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2019-11-09 15:09:52 +0900

    inicial commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root to: 'posts#index'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :posts, only: [:index, :new, :create]
+end
