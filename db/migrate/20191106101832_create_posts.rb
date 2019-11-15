7a49658429f4180057d818235eab887db611bbbc
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2019-11-15 17:03:35 +0900

    update[model]

--- db/migrate/20191106101832_create_posts.rb
+++ db/migrate/20191106101832_create_posts.rb
@@ -2,6 +2,9 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.string :url
+      t.string :img
+      t.string :title
+      t.string :description
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,9 @@ ActiveRecord::Schema.define(version: 2019_11_11_110727) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "url"
+    t.string "img"
+    t.string "title"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
