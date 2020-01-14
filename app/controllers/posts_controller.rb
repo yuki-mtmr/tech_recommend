89447a7a7bcc457d80aca4a281262b472796f7a4
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2020-01-14 19:19:09 +0900

    today's recommend

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,14 @@
 class PostsController < ApplicationController
 
+  before_action :today, only: :index
+
+
   def index
     @posts = Post.includes(:user).order("created_at DESC")
     @like = Like.new
+    @recommends = session[:today]
   end
+
 
   def new
     @post = Post.new
@@ -27,6 +32,19 @@ class PostsController < ApplicationController
   def search
     @posts = Post.search(params[:keyword])
   end
+
+  def today
+    if session[:today].blank?
+      session[:today] = Post.order("RAND()").limit(3)
+      redirect_to root_path
+    end
+  end
+
+  def destroy_today
+    session[:today] = ''
+    puts "成功しました"
+  end
+
 
   private
 
